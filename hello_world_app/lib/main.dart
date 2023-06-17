8be2d2964236b32f8cb72c1d1892cfe62373c1c9
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-06-16 21:27:57 -0500

    Hello World App

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
-import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
