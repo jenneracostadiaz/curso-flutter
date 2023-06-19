67fee7fe75638848d76c848e27997c29a1f24a67
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-06-19 01:04:38 -0500

    Yes No  Maybe App

---
+++ yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hello World',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+        _ImageBuble(),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
+
+class _ImageBuble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+            'https://yesno.wtf/assets/no/19-2062f4c91189b1f88a9e809c10a5b0f0.gif',
+            width: size.width * 0.7,  
+            height: 150,
+            fit: BoxFit.cover,
+          ));
+  }
+}
